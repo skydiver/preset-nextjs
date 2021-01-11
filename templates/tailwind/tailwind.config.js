4fde39c890fb45e4dd44ba6f55d0094a2f3509da
Martín M <skydiver@users.noreply.github.com>
2021-01-11 15:21:08 -0300

    updated tailwind config file

--- templates/tailwind/tailwind.config.js
+++ templates/tailwind/tailwind.config.js
@@ -1,11 +1,11 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
-      }
-    }
+    extend: {},
   },
-};
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
