0f0c3752a2f50828176f7abc5fc6210ec9253ef9
Martín M <skydiver@users.noreply.github.com>
2021-03-02 21:41:42 -0300

    update config

--- templates/tailwind/pages/index.js
+++ templates/tailwind/pages/index.js
@@ -1,14 +1,7 @@
-import Head from 'next/head';
-
 const Home = () => (
-  <>
-    <Head>
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
-    <div className="w-screen h-screen flex items-center justify-center">
-      <div>Next.js + Tailwind CSS</div>
-    </div>
-  </>
+  <div className="w-screen h-screen flex items-center justify-center">
+    <div>Next.js + Tailwind CSS</div>
+  </div>
 );
 
 export default Home;
